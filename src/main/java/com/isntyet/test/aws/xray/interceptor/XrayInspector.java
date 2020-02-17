7425908876cb5ed30d2403d5cc89e8a134801419
isntyet <isntyet@gmail.com>
2020-02-17 23:53:36 +0900

    action test
    action test

--- src/main/java/com/isntyet/test/aws/xray/interceptor/XrayInspector.java
+++ src/main/java/com/isntyet/test/aws/xray/interceptor/XrayInspector.java
@@ -3,23 +3,26 @@ package com.isntyet.test.aws.xray.interceptor;
 import com.amazonaws.xray.entities.Subsegment;
 import com.amazonaws.xray.spring.aop.AbstractXRayInterceptor;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
-@Aspect
+//@Aspect
 @Component
-public class XRayInspector extends AbstractXRayInterceptor {
+public class XrayInspector extends AbstractXRayInterceptor {
     @Override
     protected Map<String, Map<String, Object>> generateMetadata(ProceedingJoinPoint proceedingJoinPoint, Subsegment subsegment) {
         return super.generateMetadata(proceedingJoinPoint, subsegment);
     }
 
     @Override
-//    @Pointcut("@within(com.amazonaws.xray.spring.aop.XRayEnabled) && bean(*Controller)")
-    @Pointcut("within(com.isntyet.test.aws.xray.controller.XrayController)")
+    @Pointcut("@within(com.amazonaws.xray.spring.aop.XRayEnabled) && bean(*)")
+//    @Pointcut("within(com.isntyet.test.aws.xray.controller.XrayController)")
     public void xrayEnabledClasses() {}
+
+
 
 }
